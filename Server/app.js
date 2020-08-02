bb36853520852c7520da94d24ce52e6432fd5e2b
tomertro <42818974+tomertro@users.noreply.github.com>
2020-08-02 18:17:23 +0300

    server

--- Server/app.js
+++ Server/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+//var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mailsenderRouter = require('./routes/mailsender');
 var contactController =  require('./routes/contactController');
@@ -15,7 +15,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname,'../src/index.html'));
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
 //configure the Express middleware to accept CORS requests and parse request body into JSON
 //app.use(ra origin: "*" }));
 app.use(cors())
@@ -28,13 +28,13 @@ app.use(cookieParser());
 app.use(express.static(__dirname + '/dist/my-portfolio'));
 //app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/sendmail', mailsenderRouter);
 app.use('/contact',contactController)
 //let p = path.join(__dirname,'../src/index.html');
-//app.get('/*', function(req,res) { 
-//res.sendFile(path.join(__dirname,'../src/index.html'));});
+app.get('/*', function(req,res) { 
+res.sendFile(path.join(__dirname,'../src/index.html'));});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -48,7 +48,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  //res.render('error');
 });
 
 module.exports = app;
