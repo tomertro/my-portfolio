b3a1527d1f665637141d35dcf4305cbdeaa529ba
tomertro <42818974+tomertro@users.noreply.github.com>
2020-08-02 13:12:29 +0300

    fix

--- Server/app.js
+++ Server/app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(__dirname + '/dist/my-porfolio'));
+app.use(express.static(__dirname + '/dist/my-portfolio'));
 //app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
