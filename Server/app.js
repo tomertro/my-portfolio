d9f22517a8dd17563946ae5069989d6ee2810f45
tomertro <42818974+tomertro@users.noreply.github.com>
2020-08-02 11:27:40 +0300

    server launch index.html

--- Server/app.js
+++ Server/app.js
@@ -30,7 +30,10 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/sendmail', mailsenderRouter);
 app.use('/contact',contactController)
-
+//let p = path.join(__dirname,'../src/index.html');
+app.get('/*', function(req,res) {
+ 
+  res.sendFile(path.join(__dirname,'../src/index.html'));});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'my-portfoilo';
+  constructor() {
+    console.log(environment.production); // Logs false for default environment
+  }
 }
